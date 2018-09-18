00b3e8bc6d0c2d5b9c7d4324588def34e8162b12
GUSDANMAQ <gusdanmaq@student.gu.se>
2018-09-18 22:27:54 +0200

    get user

--- controller/user.js
+++ controller/user.js
@@ -2,29 +2,42 @@ var express = require('express');
 var router = express.Router();
 var user = require('../models/user');
 
-var users = [];
+
 
 app.post('/user', function(req, res, next){
     var new_user = {
         "id": users.length,
         "name": req.body.name,
         "city": req.body.city,
+        "location": req.body.location,
         "isDogOwner": req.body.isDogOwner,
     };
     user.save(function(err){
         if(err){ return next(err);}
         res.status(201).json(user);
+        res.send('User was created succesfully');
 
     })
 })
-
-app.get('/user', function (req, res){
-    res.json({"data": user})
+app.get('/user', function(req,res, next){
+    user.find(function(err, user){
+        if(err){return next(err);}
+        res.json({"data": user});
+    })
 })
 
-app.get('/user/:id', function(req, res){
-    res.json(user[req.params.id]);
-});
+app.get('/user:id', function(req,res,next){
+    user.findById(req.params.id), function(err, user){
+        if(err) return next(err);
+        if(user == null){
+            return res.status(404).json(
+                {"message": "User not found"}
+            );
+        }
+    }
+})
+
+
 
 
 module.exports = router
