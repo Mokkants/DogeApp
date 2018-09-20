f6568b1daac8e2e51b98d9d081b9c73a81cb68d2
duykamke <gusngudu@student.gu.se>
2018-09-20 22:12:37 +0200

    all post CRUD functions

--- routes/post.route.js
+++ routes/post.route.js
@@ -5,6 +5,7 @@ const post_controller = require('../controllers/post.controller');
 
 router.get('/:id', post_controller.post_details);
 router.get('/', post_controller.post_all_details);
-router.post('/create', post_controller.post_create);
+router.post('/', post_controller.post_create);
 router.put('/:id', post_controller.post_add);
+router.delete('/:id', post_controller.post_delete);
 module.exports = router;
