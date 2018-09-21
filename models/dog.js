792befaafcc3cc5d37c6274bbc623d33e4e8c78f
RoseBud13 <32515593+RoseBud13@users.noreply.github.com>
2018-09-21 10:18:15 +0200

    dogModel schema update

--- models/dog.js
+++ models/dog.js
@@ -1,16 +1,19 @@
 module.exports = function(mongoose){
 
 
-var Dog = new mongoose.Schema({
+var dogModel = new mongoose.Schema({
 
     dog_id: {
         type: Number,
         required: true
     },
-    /*owner: {
+    owner: {
         type: Schema.Types.objectId,
-        //foreign key
-    },*/
+        key: 'userModel',//The object can be accessed under the property 'userModel'
+        keySource: 'user_id', // The object will be fetched using the value supplied under the property 'user_id'
+        keyDestination: 'user_id',// The object will be serialized to the property 'user_id'
+        relatedModel: userModel,
+    },
     name: {
         type: String,
         required: true,
@@ -31,6 +34,6 @@ var Dog = new mongoose.Schema({
 
 });
 
-return mongoose.model('Dog', Dog);
+return mongoose.model('Dog', dogModel);
 
 }
