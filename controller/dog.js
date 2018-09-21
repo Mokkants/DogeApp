78d420e3215f4b88e16f909f0403d47b4f14eeab
RoseBud13 <32515593+RoseBud13@users.noreply.github.com>
2018-09-21 10:33:48 +0200

    findByOwner function added

--- controller/dog.js
+++ controller/dog.js
@@ -30,6 +30,18 @@ app.get('/dog', function(req, res, next){
     })
 })
 
+app.get('/dog:owner', function(req, res, next){
+    dog.findByOwner(req.params.user_id, function(err, dog){
+        if(err) return next(err);
+        if(dog == null){
+            return releaseEvents.status(404).json(
+                {"message": "Dog not found"}
+            );
+        }
+        res.json(dog);
+    })
+})
+
 app.delete('/dog:dog_id', function(req, res, next){
     dog.findOneAndDelete({id: req.params.dog_id}, function(err, dog){
         if(err) {
