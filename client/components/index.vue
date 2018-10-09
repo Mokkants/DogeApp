b5acec0b424e5c95eb30a48f75462bc704000463
RoseBud13 <32515593+RoseBud13@users.noreply.github.com>
2018-10-09 23:37:08 +0200

    add index page

---
+++ client/components/index.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+    <div class="row">
+        <div class="col-sm-4"></div>
+        <div class="col-sm-4" align="center">
+            <img src="resources/images/logo.png" alt="Doge" style="width:120px">
+        </div>
+        <div class="col-sm-4"></div>
+    </div>
+
+    <h2 align="center">Welcome to Doge!</h2>
+
+    <table  style="width:468px;height:262px;background-color: gray" align="center">
+        <tr>
+            <td>
+            <div class="form-group col-sm-12" align="center">
+                <button type="button" style="width:100px; height: 80px">
+                    <a href="login.html">Log In</a>
+                </button>
+            </div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+            <div class="form-group col-sm-12" align="center">
+                <button type="button" style="width:100px; height: 80px">
+                    <a href="register.html">Sign Up</a>
+                </button>
+            </div>
+            </td>
+        </tr>
+    </table>
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
